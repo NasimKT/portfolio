6ffe933ce276076e4aed37bb6ceb2592d7e9d597
nasim-k-t <abdulnasimkt@gmail.com>
2023-09-02 12:37:07 +0530

    Done Editing

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ window.addEventListener("resize", function() {
     nav.classList.remove("sticky");
   }
 });
+
+function validate() {
+  var fields = [
+      { id: "name", err_id: "er_name" },
+      { id: "email", err_id: "er_email" },
+      { id: "phone", err_id: "er_phone" },
+      { id: "textarea", err_id: "er_desc"}
+  ];
+
+  var isValid = true;
+
+  fields.forEach(function (field) {
+      var inputElement = document.getElementById(field.id);
+      var errorElement = document.getElementById(field.err_id);
+
+      if (inputElement.value.length === 0) {
+          inputElement.style.border = "1px solid red";
+          errorElement.style.display = "block";
+          isValid = false;
+      } else {
+          inputElement.style.border = "none";
+          errorElement.style.display = "none";
+      }
+  });
+
+  if(isValid!=false){
+    alert('Succesful');
+  }
+  return isValid;
+}
